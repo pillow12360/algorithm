c2612b8cd0c9d6e2584e3b5f9f1bcc704b769dcb
pillow12360 <ehdcks1256@naver.com>
2023-05-23 20:31:53 +0900

    19_modify

--- dong_chan/level1/19.문자열다루기기본(실패).py
+++ dong_chan/level1/19.문자열다루기기본(실패).py
@@ -1,18 +1,28 @@
+# 캐치 트라이
 # def solution(s):
 #     answer = True
-
-#     for i in s:
-#         if  int(s):
-
+    
+#     try:
+#         s = int(s)
+#     except:
+#         answer = False
+        
+#     if not len(str(s))==4 or len(str(s))==6:
+#         answer = False 
 
 #     return answer
 
-# s = "1234"
+def solution(s):
+    
+    try:
+        int(s)
+    except:
+        return False
 
-# print(solution(s))
 
-a="a"
-if format():
-    print(a)
-else:
-    print(-1)
+    return len(s)==4 or len(s)==6
+
+
+s = "1234"
+
+print(solution(s))
