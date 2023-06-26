6e5484c0623eb8ccb8fc5f342db2260aea4cc382
pillow12360 <ehdcks1256@naver.com>
2023-06-26 14:32:58 +0900

    0626 jungong

--- dong_chan/백준/Danymic_Programming/에레토스테네스의체.py
+++ dong_chan/백준/Danymic_Programming/에레토스테네스의체.py
@@ -1,25 +1,19 @@
-# def eratosthenes(N):
-#     list1 =list(range(2,N+1))
+def eratosthenes(N):
+    set1 =set(range(2,N+1))
+    for i in range(1,int(N**(1/2))+1):
+        if i in set1:
+            set1 -= set(range(2*i,N+1,i))
+    return set1
 
-#     route = N**(1/2)
-    
-    
-    
-
-
-
-# N = 100
-# print(eratosthenes(N))
-
-
-
+N = 100
+print(eratosthenes(N))
 
 def sieve_of_eratosthenes(n):
     primes = set(range(2, n+1))
     
-    for i in range(2, int(n**0.5) + 1):
-        if i in primes:
-            primes -= set(range(2*i, n+1, i))
+    for i in range(2, int(n**0.5) + 1): # 1부터 n의 제곱근까지 반복
+        if i in primes: 
+            primes -= set(range(2*i, n+1, i)) # i의 배수를 모두 차집합으로 제거
     
     return primes
 
