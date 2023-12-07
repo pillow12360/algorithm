f3827ebb67b2378678956e281a0213b161e080f7
pillow12360 <ehdcks1256@naver.com>
2023-12-07 21:18:12 +0900

    그리디-강의실_배정

--- dong_chan/Python/동아리/12월1주차_그리디/강의실배정.py
+++ dong_chan/Python/동아리/12월1주차_그리디/강의실배정.py
@@ -1,19 +1,29 @@
-from heapq import heapify, heappop, heappush
+# 12월 7일 목요일
+# 골드 5 그리디
+# 우선순위 큐
+# 정렬 sorted
+# heap 힙
 
-# 12월 04일 골드5 그리디
-
-# 강의실 배정
+import heapq
 
 N = int(input())
+
 time = []
+
 for _ in range(N):
     time.append(list(map(int, input().split())))
 
-heapify(time)
+time = sorted(time)
 
-gang = 0
-gang_list = []
+room = []  # 배정된 강의실
 
-for i in range(N):
-    [a, b] = heappop(time)
-    print(a, b)
+heapq.heappush(room, time[0][1])  # 처음 강의의 끝나는 시간을 우선순위 큐에 저장
+
+for i in range(1, N):  # 두번째 강의부터 시작
+    if time[i][0] < room[0]:  # 현재 강의실의 끝나는 시간보다. 다음 회의 시작시간이 빠르면,
+        heapq.heappush(room, time[i][1])
+    else:
+        heapq.heappop(room)  # 기존의 강의를 대체하고 새로운 강의로 끝나는 시간 변경을 휘해 pop후
+        heapq.heappush(room, time[i][1])  # push 끝나는 시간을 푸쉬
+
+print(len(room))  # 결론적으로 힙에 남아 있는 시간은 배정된 강의실에서 가장 늦게 끝나는 시간이 남는다.
