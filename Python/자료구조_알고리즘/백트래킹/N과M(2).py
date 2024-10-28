8518b157f272b28649fe95db01db0762bc6fe7f3
pillow12360 <ehdcks1256@naver.com>
2024-10-28 14:14:51 +0900

    backtracking: N과 M(2) 실버3

---
+++ Python/자료구조_알고리즘/백트래킹/N과M(2).py
@@ -0,0 +1,20 @@
+# 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+n, m = map(int,input().split())
+
+num_list = []
+
+def backtrack(num_list, m):
+    # 현재 수열의 길이가 m이면 출력하고 종료
+    if len(num_list) == m:
+        print(" ".join(map(str, num_list)))  # 공백으로 구분하여 출력
+        return
+
+    # 1부터 n까지의 숫자를 탐색
+    for i in range(1, n + 1):
+        if not num_list or (i not in num_list and num_list[-1] < i):  # 마지막 숫자보다 큰 숫자만 선택
+            num_list.append(i)  # 숫자를 추가
+            backtrack(num_list, m)  # 재귀적으로 다음 숫자를 선택
+            num_list.pop()  # 백트래킹: 선택한 숫자를 제거하고 돌아가기
+
+backtrack(num_list, m)
